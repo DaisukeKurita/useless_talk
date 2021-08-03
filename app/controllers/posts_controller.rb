55509934fc5d2bc34ca6ee9825754d8664af797d
栗田大輔 <daikons.msn36@gmail.com>
2021-08-03 23:21:25 +0900

    edit.html.erbの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,19 @@ class PostsController < ApplicationController
     redirect_to new_post_path
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
   private
   def post_params
     params.require(:post).permit(:content)
